5ee8d1cb2f2c34ecabe512bb960e46139c096bc7
Mintra Phophaijit <bluembc@gmail.com>
2019-10-27 22:31:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
-def new_hash[new_hash]
- 
-end
+def new_hash["new_hash"]
+ new_hash["new_hash"] += 1
+else
+  puts new_hash{"new_hash"]
+  end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
