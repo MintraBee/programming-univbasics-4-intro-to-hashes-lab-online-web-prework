051a84b30e83e81a18ed10c0a2fbfd0134ba029c
Mintra Phophaijit <bluembc@gmail.com>
2019-10-27 22:41:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,24 +3,23 @@ def new_hash["new_hash"]
   end
 
 def my_hash
-  {
-
+  food = {breakfast: "ham": lunch: "hotdog": dinner: "padthai"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+hash = { :name => "Mintra Phophaijit"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  return = { :id =>2 }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  {key => value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+ hash[key]
 end
 
 def update_counting_hash(hash, key)
